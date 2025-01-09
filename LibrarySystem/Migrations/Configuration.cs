3132631da29811c74e6b7e322bcc291742e933e1
MikaelGille <mikael.gille@gmail.com>
2025-01-09 19:37:08 +0100

    Added database migration

---
+++ LibrarySystem/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace LibrarySystem.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LibrarySystem.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "LibrarySystem.AppDbContext";
+        }
+
+        protected override void Seed(LibrarySystem.AppDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
